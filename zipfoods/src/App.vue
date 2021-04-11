21d8be40251c675371ffa970cd39e57a71eadc92
Jared Mimms <layermims@gmail.com>
2021-04-11 11:12:03 -0700

    ZipFoods Update

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img
+      alt="ZipFoods logo"
+      id="logo"
+      src="@/assets/images/zipfoods-logo.png"
+    />
+
+    <nav>
+      <ul>
+        <li>
+          <router-link
+            v-for="link in links"
+            v-bind:key="link"
+            v-bind:to="paths[link]"
+            >{{ link }}</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { products } from "@/common/products.js";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      products: products,
+
+      /* Store links in an array to maintain order */
+      links: ["home", "products", "categories"],
+
+      /* Map links to the appropriate component */
+      paths: {
+        home: "/",
+        products: "/products",
+        categories: "/categories",
+      },
+    };
+  },
+};
 </script>
 
+<style src='@/assets/css/zipfoods.css'></style>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="product-page">
+    <div v-if="product">
+      <h1>Product: {{ id }}</h1>
+      <div class="name">{{ product.name }}</div>
+      <img
+        class="thumb"
+        v-bind:src="require('@/assets/images/products/' + product.id + '.jpg')"
+      />
+
+      <div class="price">${{ product.price }}</div>
+      <p class="description">{{ product.description }}</p>
+    </div>
+    <div v-else>
+      <h2>Product {{ id }} not found</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { products } from "@/common/products.js";
+
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  computed: {
+    product() {
+      var product;
+      var id = this.id;
+      products.forEach(function (producted) {
+        if (producted.id == id) {
+          product = producted;
+        }
+      });
+      return product;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#product-page {
+  border: 1px solid var(--silver);
+  text-align: center;
+  padding: 15px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30%;
+  min-width: 300px;
+}
+
+.name {
+  height: 50px;
+  font-size: 2rem;
+  margin: 5px 0 10px 0;
+  vertical-align: baseline;
+}
+
+.thumb {
+  width: 75%;
+  max-width: 300px;
+  border-radius: var(--radius);
+}
+
+.description {
+  margin: auto;
+  text-align: left;
+  font-style: italic;
+  line-height: 1.5;
+}
+
+.price {
+  font-family: var(--serif-font);
+  font-weight: bold;
+  font-size: 2rem;
+  padding: 10px;
+}
+</style>
